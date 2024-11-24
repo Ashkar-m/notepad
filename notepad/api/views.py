97fe9d83a8cbd9b8f3b15e985eb6dfcfdf7d5f94
Ashkar-m <151915974+Ashkar-m@users.noreply.github.com>
2024-11-24 19:05:43 +0530

    Added new views to get Notes and single note

--- notepad/api/views.py
+++ notepad/api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Note
+from .serializers import NoteSerializer
+
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -38,3 +41,18 @@ def getRoutes(request):
         },
     ]
     return Response(routes)
+
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getNote(request, pk):
+    
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=False)
+    return Response(serializer.data)
