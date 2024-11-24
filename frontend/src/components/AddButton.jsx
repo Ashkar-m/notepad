2f81acfe6e7a903195dc94127c56e152aa77f360
Ashkar-m <151915974+Ashkar-m@users.noreply.github.com>
2024-11-25 01:50:04 +0530

    New pages and components are added

---
+++ frontend/src/components/AddButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AddButton = () => {
+  return (
+    <div>
+        <Link to="/note/new" className="floating-button">
+            Add
+        </Link>
+      
+    </div>
+  )
+}
+
+export default AddButton;
